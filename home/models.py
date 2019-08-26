851e8c6b45c52a356c39cd0fd55782329e25f9fb
Toby <tdudleyoffice@gmail.com>
2019-08-26 13:42:18 -0500

    homepage created, limited to 1

--- home/models.py
+++ home/models.py
@@ -1,7 +1,18 @@
 from django.db import models
 
 from wagtail.core.models import Page
+from wagtail.admin.edit_handlers import FieldPanel
 
 
 class HomePage(Page):
-    pass
+    """Home page model"""
+    templates = "home/templates/home_page.html"
+    banner_title = models.CharField(max_length=100, blank=False, null=True)
+    max_count = 1
+    content_panels = Page.content_panels + [
+        FieldPanel("banner_title")
+    ]
+
+    class Meta:
+        verbose_name = "Home Page"
+        verbose_name_plural = "Home pages"
