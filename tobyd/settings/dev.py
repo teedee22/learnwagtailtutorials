b60aa82022feb078bbdb60fe66604b07382f0cd7
Toby <tdudleyoffice@gmail.com>
2019-08-26 14:47:59 -0500

    installed django debug toolbar

--- tobyd/settings/dev.py
+++ tobyd/settings/dev.py
@@ -7,10 +7,19 @@ DEBUG = True
 SECRET_KEY = '7w$qbg$a23$@$tq0_kka4*iklpnyjmt7f(d+=w4wfmw&)6$swu'
 
 # SECURITY WARNING: define the correct hosts in production!
-ALLOWED_HOSTS = ['*'] 
+ALLOWED_HOSTS = ['*']
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+INSTALLED_APPS = INSTALLED_APPS + [
+    'debug_toolbar',
+]
+
+MIDDLEWARE = MIDDLEWARE + [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = ("127.0.0.1", "172.17.0.1")
 
 try:
     from .local import *
