f6b1f894f4252c5f92f20fe2de1513a1e02f7e59
Toby <tdudleyoffice@gmail.com>
2019-08-26 20:11:28 -0500

    added two more streamfields

--- flex/models.py
+++ flex/models.py
@@ -15,7 +15,9 @@ class FlexPage(Page):
 
     content = StreamField(
         [
-            ("title_and_text", blocks.TitleAndTextBlock())
+            ("title_and_text", blocks.TitleAndTextBlock()),
+            ("full_richtext", blocks.RichTextBlock()),
+            ("simple_richtext", blocks.SimpleRichTextBlock()),
         ],
         null=True,
         blank=True
--- streams/blocks.py
+++ streams/blocks.py
@@ -13,3 +13,27 @@ class TitleAndTextBlock(blocks.StructBlock):
         template = "streams/title_and_text_block.html"
         icon = "edit"
         label = "Title & Text"
+
+
+class RichTextBlock(blocks.RichTextBlock):
+    """Richtext with all the features"""
+
+    class Meta:  # noqa
+        template = "streams/richtext_block.html"
+        icon = "doc-full"
+        label = "Full RichText"
+
+
+class SimpleRichTextBlock(blocks.RichTextBlock):
+    """Richtext without (limited) all the features"""
+
+    def __init__(
+        self, required=True, help_text=None, editor="default", features=None, **kwargs
+    ):  # noqa
+        super().__init__(**kwargs)
+        self.features = ["bold", "italic", "link"]
+
+    class Meta:  # noqa
+        template = "streams/richtext_block.html"
+        icon = "edit"
+        label = "Simple RichText"
