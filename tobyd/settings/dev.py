603869af19fe8ce372fa2a6267f1cd0d151d17af
Toby <tdudleyoffice@gmail.com>
2019-09-17 19:19:16 -0500

    add email functionality

--- tobyd/settings/dev.py
+++ tobyd/settings/dev.py
@@ -9,7 +9,15 @@ SECRET_KEY = '7w$qbg$a23$@$tq0_kka4*iklpnyjmt7f(d+=w4wfmw&)6$swu'
 # SECURITY WARNING: define the correct hosts in production!
 ALLOWED_HOSTS = ['*']
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+#EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+STATIC_URL = '/static/'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = os.environ['MG_HOST']
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ['MG_ADDRESS']
+EMAIL_HOST_PASSWORD =os.environ['MG_PASSWORD']
+EMAIL_USE_TLS = True
 
 INSTALLED_APPS = INSTALLED_APPS + [
     'debug_toolbar',
